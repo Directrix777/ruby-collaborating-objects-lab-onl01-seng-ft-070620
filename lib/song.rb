83b54a002b3e74a5ece265155b0107853048aa4e
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-07-20 17:24:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,4 +18,13 @@ class Song
     return @artist.name
   end
 
+  def artist_name=(name)
+    @artist = Artist.find_or_create_by_name(name)
+  end
+
+  def new_by_filename(filename)
+  end
+
+
+
 end
